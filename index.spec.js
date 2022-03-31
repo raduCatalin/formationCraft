c2947a5e4853e0b3e43e463afc25f8c0e7086460
horeanu radu <horeanu.raducatalin.ext@eulerhermes.com>
2022-03-31 17:47:52 +0200

    add github-actions.yml

--- index.spec.js
+++ index.spec.js
@@ -1,6 +1,18 @@
 const compute = require("./index");
 
 describe("Basic test", () => {
+  it("should return a number", () => {
+    // Arrange
+    const input = "1";
+    const output = "1";
+
+    // Act
+    const result = compute(input);
+
+    // Assert
+    expect(result).toEqual(output);
+  });
+
   it("should do something when having some inputs", () => {
     // Arrange
     const input = "1";
