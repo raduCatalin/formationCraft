2b0a070522d2a3ea1a35ffa6525d363bc4bbabb0
horeanu radu <horeanu.raducatalin.ext@eulerhermes.com>
2022-04-01 09:43:18 +0200

    add exercice

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
-const compute = (n) => {
-    return n;
-}
+//https://github.com/newlight77/bootstraps
+const equivalences = [
+  { number: 3, label: "Foo" },
+  { number: 5, label: "Bar" },
+  { number: 7, label: "Qix" },
+];
 
-module.exports = compute
+const compute = (input) => {
+  const number = parseFloat(input);
+  let res = "";
+
+  if (!isNaN(number)) {
+    res = getDivisibleEquivalence(number, res);
+  }
+
+  res = getReplacedEquivalence(input, res);
+
+  if (!res) {
+    return input;
+  }
+
+  return res;
+};
+
+const getDivisibleEquivalence = (number, res) => {
+  equivalences.forEach((equivalence) => {
+    if (number % equivalence.number === 0) {
+      res += equivalence.label;
+    }
+  });
+
+  return res;
+};
+
+const getReplacedEquivalence = (input, res) => {
+  equivalences.forEach((equivalence) => {
+    const founded = input.match(new RegExp(equivalence.number, "g")) || [];
+    founded.forEach((item) => {
+      res += equivalence.label;
+    });
+  });
+
+  return res;
+};
+
+module.exports = compute;
--- index.spec.js
+++ index.spec.js
@@ -1,7 +1,7 @@
 const compute = require("./index");
 
 describe("Basic test", () => {
-  it("should return a number", () => {
+  it("should return 1 when given 1", () => {
     // Arrange
     const input = "1";
     const output = "1";
@@ -13,10 +13,70 @@ describe("Basic test", () => {
     expect(result).toEqual(output);
   });
 
-  it("should do something when having some inputs", () => {
+  it("should return 2 when given 2", () => {
     // Arrange
-    const input = "1";
-    const output = "1";
+    const input = "2";
+    const output = "2";
+
+    // Act
+    const result = compute(input);
+
+    // Assert
+    expect(result).toEqual(output);
+  });
+
+  it("should return 'FooFoo' when given 3", () => {
+    // Arrange
+    const input = "3";
+    const output = "FooFoo";
+
+    // Act
+    const result = compute(input);
+
+    // Assert
+    expect(result).toEqual(output);
+  });
+
+  it("should return 'BarBar' when given 5", () => {
+    // Arrange
+    const input = "5";
+    const output = "BarBar";
+
+    // Act
+    const result = compute(input);
+
+    // Assert
+    expect(result).toEqual(output);
+  });
+
+  it("should return 'QixQix' when given 7", () => {
+    // Arrange
+    const input = "7";
+    const output = "QixQix";
+
+    // Act
+    const result = compute(input);
+
+    // Assert
+    expect(result).toEqual(output);
+  });
+
+  it("should return 'Foo' when given 'toto3titi' ", () => {
+    // Arrange
+    const input = "toto3titi";
+    const output = "Foo";
+
+    // Act
+    const result = compute(input);
+
+    // Assert
+    expect(result).toEqual(output);
+  });
+
+  it("should return empty string when given an empty string", () => {
+    // Arrange
+    const input = "";
+    const output = "";
 
     // Act
     const result = compute(input);
